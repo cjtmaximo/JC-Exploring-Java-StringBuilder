7c0b9aa620ef5700085e2ee00d8a2218971957ee
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-07 15:01:08 +0800

    feat: finish Exercise 2 - Inserting Content at a Position

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,10 @@ public class Main {
         System.out.println(sb);
         sb.append(" Welcome to ").append("2025").append(".");
         System.out.println(sb);
+
+        // Exercise 2 - Inserting Content at a Position
+        StringBuilder sb2 = new StringBuilder("Java is great!");
+        sb2.insert(8, "really ");
+        System.out.println(sb2);
     }
 }
